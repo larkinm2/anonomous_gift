d9db1ab8481352b59d745a313213f72373b3922a
larkinm2 <larkinm2@gmail.com>
2014-10-29 19:53:59 -0400

    adds role column to users

--- db/migrate/20141006174453_add_role_column_to_users.rb
+++ db/migrate/20141006174453_add_role_column_to_users.rb
@@ -1,7 +1,7 @@
 class AddRoleColumnToUsers < ActiveRecord::Migration
 
   def change
-        add_column :users, :role, :string, default: "user"
+    add_column :users, :role, :string, default: "user"
   end
 
 end
